662d77d9d93f028fa3a5fc037a5bf1c09a282093
Paul Guerre <pguerreftb@gmail.com>
2021-11-07 16:39:52 +0100

    Updating indentation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,31 +23,51 @@ namespace Wake_On_Lan_Lite
             this.devices.Add(new Device() { NAME = "Server2", ADDRESS = "zz:yy:xx:ww:vv:uu" });
         }
 
+        //Refresh dataGrid data
+        public void dataRefresh()
+        {
+            dataGrid.ItemsSource = null;
+            dataGrid.ItemsSource = this.devices;
+        }
+
+        //Add data to dataGrid and file
+        public void addData(string name, string address)
+        {
+            if(address == null)
+            {
+                //fileControl.add...
+                this.devices.Add(new Device() { NAME = name, ADDRESS = "" });
+            }
+            else
+            {
+                //fileControl.update...
+                this.devices[dataGrid.Items.Count - 2].ADDRESS = address;
+            }
+            dataRefresh();
+        }
+
         public void dg_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
         {
             if(dataGrid.CurrentColumn != null)
             {
+                TextBox t = e.EditingElement as TextBox;
+
                 if (dataGrid.CurrentColumn.DisplayIndex == dataGrid.Columns.Count - 2)
                 {
-                    TextBox t = e.EditingElement as TextBox;
-                    this.devices.Add(new Device() { NAME = t.Text.ToString(), ADDRESS = "" });
-                    dataGrid.ItemsSource = null;
-                    dataGrid.ItemsSource = this.devices;
+                    addData(t.Text.ToString(), null);
                     dataGrid.CurrentCell = new DataGridCellInfo(dataGrid.Items[dataGrid.Items.Count - 2], dataGrid.Columns[dataGrid.Columns.Count - 1]);
-                    dataGrid.BeginEdit();
                 }
                 else
                 {
-                    TextBox t = e.EditingElement as TextBox;
-                    this.devices[dataGrid.Items.Count - 2].ADDRESS = t.Text.ToString();
-                    dataGrid.ItemsSource = null;
-                    dataGrid.ItemsSource = this.devices;
+                    addData(null, t.Text.ToString());
                     dataGrid.CurrentCell = new DataGridCellInfo(dataGrid.Items[dataGrid.Items.Count - 1], dataGrid.Columns[dataGrid.Columns.Count - 2]);
-                    dataGrid.BeginEdit();
                 }
+
+                dataGrid.BeginEdit();
             }
         }
 
+        //When click "add" button
         public void showAdd(object sender, RoutedEventArgs e)
         {
             dataGrid.CurrentCell = new DataGridCellInfo(dataGrid.Items[dataGrid.Items.Count - 1], dataGrid.Columns[dataGrid.Columns.Count - 2]);
